e3413a014fc0fc6fca8af63c8c75ec747ecd529f
Adnan K. Radwan <54576312+adnan-radwan@users.noreply.github.com>
2026-02-23 16:54:51 -0700

    Update category.js

--- ad-view/category.js
+++ ad-view/category.js
@@ -35,3 +35,19 @@ fetch("https://adnan-radwan.github.io/ad-view/adsDB.js")
     });
 
   });
+
+const CATEGORY_PAGE = "https://www.adnan-radwan.net/p/category.html";
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  document.querySelectorAll("[data-cat]").forEach(btn => {
+    btn.addEventListener("click", function () {
+      const slug = this.getAttribute("data-cat");
+      if (slug) {
+        const url = CATEGORY_PAGE + "?cat=" + encodeURIComponent(slug);
+        window.open(url, "_blank");
+      }
+    });
+  });
+
+});
