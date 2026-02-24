e1555a4d16ac33f204bedfd778c7b989c430f5da
Adnan K. Radwan <54576312+adnan-radwan@users.noreply.github.com>
2026-02-23 17:01:53 -0700

    Update category.js

--- ad-view/category.js
+++ ad-view/category.js
@@ -5,37 +5,6 @@ bar.innerHTML = categoriesDB.map(cat => `
     ${cat.icon} ${cat.name_ar}
   </a>
 `).join("");
-
-fetch("https://adnan-radwan.github.io/ad-view/adsDB.js")
-  .then(() => {
-
-    const container = document.getElementById("categoriesContainer");
-
-    categoriesDB.forEach(cat => {
-
-      const ads = window.adsDB.filter(a => a.category === cat.slug);
-
-      if (ads.length === 0) return;
-
-      const section = document.createElement("div");
-      section.innerHTML = `
-        <div class="section-title">${cat.icon} ${cat.name_ar} (${ads.length})</div>
-        <div class="grid">
-          ${ads.map(ad => `
-            <a class="ad-card" href="${ad.link}">
-              <img class="ad-img" src="${ad.img}">
-              <div class="ad-title">${ad.title}</div>
-              <div class="ad-desc">${ad.desc}</div>
-            </a>
-          `).join("")}
-        </div>
-      `;
-
-      container.appendChild(section);
-    });
-
-  });
-
 const CATEGORY_PAGE = "https://www.adnan-radwan.net/p/category.html";
 
 document.addEventListener("DOMContentLoaded", function () {
